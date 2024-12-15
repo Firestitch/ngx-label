d420c0ecaea7d95e2d2e86b5b405e8789eb468e7
Raymond Gigliotti <ray@firestitch.com>
2024-12-15 08:37:01 -0500

    supported outline field

--- playground/app/playground.module.ts
+++ playground/app/playground.module.ts
@@ -1,42 +1,47 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { RouterModule, Routes } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule, Routes } from '@angular/router';
+
+
+import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
 
 import { FsExampleModule } from '@firestitch/example';
-import { FsMessageModule } from '@firestitch/message';
 import { FsLabelModule } from '@firestitch/label';
+import { FsMessageModule } from '@firestitch/message';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToastrModule } from 'ngx-toastr';
 
-import { AppMaterialModule } from './material.module';
-import { ExampleComponent, ExamplesComponent } from './components';
 import { AppComponent } from './app.component';
+import { ExampleComponent, ExamplesComponent } from './components';
+import { AppMaterialModule } from './material.module';
 
 const routes: Routes = [
   { path: '', component: ExamplesComponent },
 ];
 
 @NgModule({
-    bootstrap: [AppComponent],
-    imports: [
-        BrowserModule,
-        FsLabelModule,
-        BrowserAnimationsModule,
-        AppMaterialModule,
-        FormsModule,
-        FsExampleModule.forRoot(),
-        FsMessageModule.forRoot(),
-        ToastrModule.forRoot({ preventDuplicates: true }),
-        RouterModule.forRoot(routes, {}),
-    ],
-    declarations: [
-        AppComponent,
-        ExamplesComponent,
-        ExampleComponent
-    ],
-    providers: []
+  bootstrap: [AppComponent],
+  imports: [
+    BrowserModule,
+    FsLabelModule,
+    BrowserAnimationsModule,
+    AppMaterialModule,
+    FormsModule,
+    FsExampleModule.forRoot(),
+    FsMessageModule.forRoot(),
+    ToastrModule.forRoot({ preventDuplicates: true }),
+    RouterModule.forRoot(routes, {}),
+  ],
+  declarations: [
+    AppComponent,
+    ExamplesComponent,
+    ExampleComponent,
+  ],
+  providers: [
+    { provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: { appearance: 'outline' } },
+  ],
 })
 export class PlaygroundModule {
 }
--- src/app/components/label-field/label-field.component.ts
+++ src/app/components/label-field/label-field.component.ts
@@ -1,32 +1,40 @@
-import { FsLabelComponent } from './../label/label.component';
-import { ChangeDetectionStrategy, Component, HostBinding, Input, ContentChild } from '@angular/core';
+import { ChangeDetectionStrategy, Component, HostBinding, Input } from '@angular/core';
+
 
 @Component({
   selector: 'fs-label-field',
-  templateUrl: 'label-field.component.html',
-  styleUrls: [ 'label-field.component.scss' ],
+  templateUrl: './label-field.component.html',
+  styleUrls: ['./label-field.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class FsLabelFieldComponent {
+  
+  @Input('outline') 
+  @HostBinding('class.outline')
+  public outline = false;
+  
+  @Input('disabled') 
+  @HostBinding('class.disabled')
+  public disabled = false;
+  
+  @Input('focused') 
+  @HostBinding('class.focused')
+  public focused = false;
+  
+  @Input('hoverable') 
+  @HostBinding('class.hoverable')
+  public hoverable = false;
 
-  @Input('bottomMargin') set setBottomMargin(value) {
-    this.bottomMargin = value;
-  }
-
-  @Input('topMargin') set setTopMargin(value) {
-    this.topMargin = value;
-  }
-
-  @Input('labelMargin') set setLabelMargin(value) {
-    this.labelMargin = value;
-  }
-
+  @Input('bottomMargin') 
   @HostBinding('class.field-bottom-margin')
   public bottomMargin = true;
 
+  @Input('topMargin') 
   @HostBinding('class.field-top-margin')
   public topMargin = true;
 
+  @Input('labelMargin') 
   @HostBinding('class.field-label-margin')
   public labelMargin = true;
+
 }
