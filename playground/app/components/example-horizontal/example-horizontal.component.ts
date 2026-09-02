6e9172c940927bd5fe4e811cd9de44c46d0d0a1b
Raymond Gigliotti <ray@firestitch.com>
2026-09-02 09:14:04 -0400

    Update .gitignore, add new examples to playground components, and enhance label field styling

---
+++ playground/app/components/example-horizontal/example-horizontal.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { FsLabelComponent } from '../../../../src/app/components/label/label.component';
+import { FsLabelFieldComponent } from '../../../../src/app/components/label-field/label-field.component';
+import { FsLabelMessageComponent } from '../../../../src/app/components/label-message/label-message.component';
+
+@Component({
+    selector: 'example-horizontal',
+    templateUrl: 'example-horizontal.component.html',
+    styleUrls: ['example-horizontal.component.scss'],
+    standalone: true,
+    imports: [FsLabelComponent, FsLabelFieldComponent, FsLabelMessageComponent]
+})
+export class ExampleHorizontalComponent {
+}
--- playground/app/components/examples/examples.component.ts
+++ playground/app/components/examples/examples.component.ts
@@ -2,12 +2,19 @@ import { Component } from '@angular/core';
 import { environment } from '../../../environments/environment';
 import { FsExampleModule } from '@firestitch/example';
 import { ExampleComponent } from '../example/example.component';
+import { ExampleStackedComponent } from '../example-stacked/example-stacked.component';
+import { ExampleHorizontalComponent } from '../example-horizontal/example-horizontal.component';
 
 
 @Component({
     templateUrl: 'examples.component.html',
     standalone: true,
-    imports: [FsExampleModule, ExampleComponent]
+    imports: [
+      FsExampleModule,
+      ExampleComponent,
+      ExampleStackedComponent,
+      ExampleHorizontalComponent,
+    ]
 })
 export class ExamplesComponent {
   public config = environment;
